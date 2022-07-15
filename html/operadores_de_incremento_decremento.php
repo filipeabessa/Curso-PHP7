ca190728a2c323acc8eb3fa39f696f0ac196d7c8
filipeabessa <filipegbessa@gmail.com>
2022-07-15 17:00:18 -0300

    aula 12 - pre-incremento e pos-incremento

---
+++ html/operadores_de_incremento_decremento.php
@@ -0,0 +1,29 @@
+<?php
+$valor = 20;
+
+// pré-incremento
+echo ++$valor;
+echo "<br>";
+echo $valor;
+
+echo "<hr>";
+
+// pós-incremento
+echo $valor++;
+echo "<br>";
+echo $valor;
+
+echo "<hr>";
+
+// pré-decremento
+echo --$valor;
+echo "<br>";
+echo $valor;
+
+echo "<hr>";
+
+// pós-decremento
+echo $valor--;
+echo "<br>";
+echo $valor;
+?>
